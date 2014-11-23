762b48437e2952c139aaf243ae0a7e95f5581465
Flynn O'Connor <fsquaredesign@gmail.com>
2014-11-22 19:36:51 -0800

    Add template for front end search form

---
+++ public/partials/swiftype-search-geolocation-public-form-template.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Search Form used by shortcode for searching for posts based on Latitude and Longitude
+ *
+ * This file is used as template Search Form used by shortcode for searching for posts based on Latitude and Longitude.
+ *
+ * @since      1.0.0
+ *
+ */
+?>
+<script type="text/html" id="tmpl-swiftype-geo-form">
+	<form id="swiftype-geo-search">
+		<label for="swiftype-geo-address">
+			<input type="text" name="swiftype-geo-address" id="swiftype-geo-address" value="" placeholder="<?php _e( 'please enter your address', 'swiftype-search-geolocation' ); ?>">
+		</label>
+			<input type="submit" id="swiftype-geo-submit" value="<?php _e( 'Search', 'swiftype-search-geolocation' ); ?>">
+	</form>
+</script>
