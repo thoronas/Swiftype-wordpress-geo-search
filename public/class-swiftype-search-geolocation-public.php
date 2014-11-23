9721662bf4f5e0e93cc85ebd21349691011c6615
Flynn O'Connor <fsquaredesign@gmail.com>
2014-11-22 19:38:05 -0800

    Improvements to shortcode functionality

--- public/class-swiftype-search-geolocation-public.php
+++ public/class-swiftype-search-geolocation-public.php
@@ -86,17 +86,6 @@ class Swiftype_Search_Geolocation_Public {
 	 */
 	public function enqueue_scripts() {
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Plugin_Name_Public_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Plugin_Name_Public_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
 		
 	}
 
@@ -111,57 +100,86 @@ class Swiftype_Search_Geolocation_Public {
 	 * @return type
 	 */
 	public function render_swiftype_geolocation_shortcode( $atts ) {
-		// enqueue scripts only when shortcode is called. 
-		wp_enqueue_script( 'swiftype_geo_js', plugin_dir_url( __FILE__ ) . 'js/swiftype-search-geolocation-public.js', array( 'jquery' ), $this->version, FALSE );
-	    wp_enqueue_script( 'Swiftype-googlemaps-JS', 'https://maps.googleapis.com/maps/api/js', null, '0.1', FALSE );
-	    
-	    extract(shortcode_atts(array(
-	        'lat' 	=> '37.090240',
-	        'long' 	=> '-95.712891', 
-	        'zoom' 	=> '5', 
-	        'max'	=> '50km'
-	    ), $atts));
-
-		$api_key     = get_option( 'swiftype_api_key' );
-		$engine_slug = get_option( 'swiftype_engine_slug' );
 		
+		extract( shortcode_atts(array(
+			'lat' 			=> '',
+			'long' 			=> '', 
+			'zoom' 			=> '5', 
+			'distance'		=> '50',
+			'search_form'	=> true,
+			'post_types'	=> get_option( 'swiftype-search-geolocation-post-types' ), 
+
+		), $atts) );
+
+		// do a quick check to make sure lat & long is usable
+		if( empty($lat) || empty($long) ){
+			return; 
+		}
+
+		$this->render_swiftype_geolocation_shortcode_js(); 
+
+		// post types need to be in an array for Swiftype. 
+		$post_types = explode(',', $post_types);
+		$distance		= $distance."km";	
+		
+		$markers = $this->geo_search_swiftype($lat, $long, $distance, $post_types); 
+
+		// prep data for localization
+		$data = array(
+			"geo_lat" 			=> $lat,
+			"geo_long" 			=> $long,
+			"map_zoom"			=> $zoom,
+			"search_form"		=> $search_form,
+			"swiftype_markers"	=> $markers
+		); 
+		//output swiftype data to be used by geo scripts. 
+		wp_localize_script( 'swiftype_geo_js', 'swiftype_geo_data', $data );
+		
+		//google maps container. 
+		$output = '<div id="swiftype-map"></div>';
+		return $output; 
+	}
+
+	public function geo_search_swiftype($lat, $long, $distance, $post_types){
+		$api_key		= get_option( 'swiftype_api_key' );
+		$engine_slug	= get_option( 'swiftype_engine_slug' );
+
 		//instantiate Swiftype client to pass parameters to swiftype API
-		$client      = new SwiftypeClient();
+		$client = new SwiftypeClient();
 		$client->set_api_key( $api_key );
 
-        $query_string = '';
-        $document_type_slug = 'posts'; 
-        $params = array();
-        $params['fetch_fields[posts]'] = apply_filters('swiftype_geolocation_return_fields', array( 'geolocation','title','external_id','url' ) );
-        
-        $params['filters[posts][geolocation]'] = array(
-            "type" => "distance",
-            "max" => $max,
-            "lat" => $lat,
-            "lon" => $long 
-        );
+		// pass an empty query string for now as it is expected. To-do: allow string search with geo search. 
+		$query_string = '';
+		$document_type_slug = 'posts'; 
+		$params = array();
+		$params['filters[posts][object_type]'] = $post_types; 
+
+		// filter to change which fields are returned. To-Do: enable customization of infobox
+		$params['fetch_fields[posts]'] = apply_filters('swiftype_geolocation_return_fields', array( 'geolocation','title','external_id','url' ) );
+		
+		$params['filters[posts][geolocation]'] = array(
+			"type" => "distance",
+			"max" => $distance,
+			"lat" => $lat,
+			"lon" => $long 
+		);
 	
 		$swiftype_result = $client->search( $engine_slug, $document_type_slug, $query_string, $params );
-		$markers = $swiftype_result['records']['posts'];
-	    
-	    // prep data for localization
-	    $data = array(
-	    	"geo_lat" 			=> $lat,
-	    	"geo_long" 			=> $long,
-	    	"swiftype_markers"	=> $markers
-    	); 
-	    //output swiftype data to be used by geo scripts. 
-    	wp_localize_script( 'swiftype_geo_js', 'swiftype_geo_data', $data );
-	    
-	    //google maps container. 
-	    $output = '<div id="swiftype-map"></div>';
-	    return $output; 
-	}
+		return $swiftype_result['records']['posts'];
 
+	}
 
 	public function register_swiftype_geolocation_shortcodes(){
-	    add_shortcode( 'swiftype-geo-location', array( $this, 'render_swiftype_geolocation_shortcode' ) );
+		add_shortcode( 'swiftype-geo-location', array( $this, 'render_swiftype_geolocation_shortcode' ) );
 	}
 
-
+	public function render_swiftype_geolocation_shortcode_js(){
+		// enqueue scripts only when shortcode is called. 
+		wp_enqueue_script( 'swiftype_geo_js', plugin_dir_url( __FILE__ ) . 'js/swiftype-search-geolocation-public.js', array( 'jquery', 'wp-util' ), $this->version, true );
+		wp_enqueue_script( 'Swiftype-googlemaps-JS', 'https://maps.googleapis.com/maps/api/js', null, '0.1', FALSE );
+		add_action( "wp_footer", array( $this, "render_shortcode_form" ) ); 
+	}
+	public function render_shortcode_form(){
+		include( plugin_dir_path( __FILE__ ) . 'partials/swiftype-search-geolocation-public-form-template.php');
+	} 
 }
--- public/js/swiftype-search-geolocation-public.js
+++ public/js/swiftype-search-geolocation-public.js
@@ -1,33 +1,46 @@
-function display_map_markers(data){
-    var canvas = jQuery("#swiftype-map").get(0);
-    var geocoder, map; 
+(function( $ ) {
+	'use strict';
+	function display_map_markers(data){
+		var canvas = jQuery("#swiftype-map").get(0);
+		var geocoder, map, marker_lat, marker_long; 
+		var infowindow = new google.maps.InfoWindow();
+		if(canvas) {
 
-    if(canvas) {
+			var zoom = parseInt(swiftype_geo_data.map_zoom);
 
-        var zoom = parseInt(8);
+			var options = {
+				zoom: zoom, 
+				center: new google.maps.LatLng(swiftype_geo_data.geo_lat, swiftype_geo_data.geo_long) 
+			};
+			map = new google.maps.Map(canvas, options);
 
-        var options = {
-            zoom: zoom , 
-            center: new google.maps.LatLng(swiftype_geo_data.geo_lat, swiftype_geo_data.geo_long) 
-        };
-        map = new google.maps.Map(canvas, options);
+		}
 
-    }
+		$.each( swiftype_geo_data.swiftype_markers, function( key, val ) {
+			marker_lat = val.geolocation.lat;
+			marker_long = val.geolocation.lon;
+			var LocLatlng = new google.maps.LatLng(marker_lat,marker_long);
 
-    jQuery.each( swiftype_geo_data.swiftype_markers, function( key, val ) {
-        marker_lat = val.geolocation.lat;
-        marker_long = val.geolocation.lon;
-        var LocLatlng = new google.maps.LatLng(marker_lat,marker_long);
+			var marker = new google.maps.Marker({
+				position: LocLatlng,
+				map: map,
+			});
 
-        var marker = new google.maps.Marker({
-            position: LocLatlng,
-            map: map,
-        });
+			google.maps.event.addListener(marker, 'click', (function(marker, i) {
+				return function() {
+					var content = "<a class='swiftype-map-marker' href='"+val.url+"'>"+val.title+"</a>";
+					infowindow.setContent(content);
+					infowindow.open(map, marker);
+				}
+			})(marker, key));
+		});
+		if(swiftype_geo_data.search_form == "true"){
+			var form_content = wp.template( "swiftype-geo-form" ); 
+			$("#swiftype-map").before(form_content);
+		}
+	}
+	$(document).ready(function(){
+		display_map_markers(swiftype_geo_data);    
+	});
 
-
-    });
-}
-jQuery(document).ready(function($){
-    display_map_markers(swiftype_geo_data);    
-});
-
+})( jQuery );
