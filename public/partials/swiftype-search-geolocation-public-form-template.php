000e8e50c53c46ff919f64b706aa2a276d84ddbc
Flynn O'Connor <fsquaredesign@gmail.com>
2014-12-08 23:55:33 -0800

    Styling of front end search form.

--- public/partials/swiftype-search-geolocation-public-form-template.php
+++ public/partials/swiftype-search-geolocation-public-form-template.php
@@ -10,7 +10,7 @@
  */
 ?>
 <script type="text/html" id="tmpl-swiftype-geo-form">
-	<form id="swiftype-geo-search">
+	<form id="swiftype-geo-search" class="swiftype-geo-search-form" >
 		<label for="swiftype-geo-address">
 			<input type="text" name="swiftype-geo-address" id="swiftype-geo-address" value="" placeholder="<?php _e( 'please enter your address', 'swiftype-search-geolocation' ); ?>">
 		</label>
